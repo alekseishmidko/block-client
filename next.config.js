46490c71de3956f635a983b9bbdf4b126427cdbb
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2024-06-02 15:11:21 +0300

    2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}:path*`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
