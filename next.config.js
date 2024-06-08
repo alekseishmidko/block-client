9be184ab0d7c89e9c18bef57e01e2a9b2af87a44
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2024-06-08 21:24:20 +0300

    4

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_BASE_URL}:path*`,
+        destination: `${process.env.BASE_URL}:path*`,
       },
     ];
   },
